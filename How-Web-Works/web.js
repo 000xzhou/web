a918ec7f2c96b404cec2ccb9dcab98026f8e53dc
000xzhou <xzhou92@gmail.com>
2023-11-13 23:14:08 -0500

    part 1 of web

---
+++ How-Web-Works/web.js
@@ -0,0 +1,39 @@
+/**
+## **Part One: Solidify Terminology**
+
+In your own terms, define the following terms:
+
+- What is HTTP?
+    A port to a server
+- What is a URL?
+    A link
+- What is DNS?
+    A system that converts URL to IP address
+- What is a query string?
+    Let's you pass key-value pairs into the url
+- What are two HTTP verbs and how are they different?
+    Get - Get data from server
+    POST - Send data to server
+- What is an HTTP request?
+    Request to server from client
+- What is an HTTP response?
+    Response from server to client
+- What is an HTTP header? Give a couple examples of request and response headers you have seen.
+    To let HTTP know what you want to do
+    - Request headers: Host, User-Agent, Accept, Cookie, Cache-Control
+    - Response headers: Content-Type, Last-Modified, Set-Cookie, Cache-Control
+- What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+    change to IP with DNS
+    make a request to server
+    send back a response back to you
+    -The browser makes a DOM from that HTML, and finds any other resources needed (images, CSS, JavaScript, etc)
+    -The browser makes separate HTTP requests for those resources and receives response from the server for each
+ */
+
+/**
+## ****Part Two: Practice Tools****
+
+1. Using ***curl***, make a ***GET*** request to the *icanhazdadjoke.com* API to find all jokes involving the word “pirate”
+2. Use ***dig*** to find what the IP address is for *icanhazdadjoke.com*
+3. Make a simple web page and serve it using ***python3 -m http.server***. Visit the page in a browser.
+ */
